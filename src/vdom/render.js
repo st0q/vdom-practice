6cb7178606eea79666c03caac5cb3b03013e4b3c
st0q <tssk713@gmail.com>
2022-04-07 07:43:36 +0900

    既存のrender関数をElementNodeを生成する関数に置き換える

--- src/vdom/render.js
+++ src/vdom/render.js
@@ -1,17 +1,17 @@
-const render = (vNode) => {
+const renderElem = ({ tagName, attrs, children}) => {
     // create the element
     //   e.g. <div></div>
-    const $el = document.createElement(vNode.tagName);
+    const $el = document.createElement(tagName);
   
     // add all attributs as specified in vNode.attrs
     //   e.g. <div id="app"></div>
-    for (const [k, v] of Object.entries(vNode.attrs)) {
+    for (const [k, v] of Object.entries(attrs)) {
       $el.setAttribute(k, v);
     }
   
     // append all children as specified in vNode.children
     //   e.g. <div id="app"><img></div>
-    for (const child of vNode.children) {
+    for (const child of children) {
       $el.appendChild(render(child));
     }
   
