d70904a56d1ef239c1dfc1c85800c6ef2ad06cc9
st0q <tssk713@gmail.com>
2022-04-06 22:13:28 +0900

    childrenを追加

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ const vApp = createElement('div', {
   attrs: {
     id: 'app',
   },
+  children: [
+    createElement('img', {
+      attrs: {
+        src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif',
+      },
+    }),
+  ],
 });
 
 console.log(vApp);
